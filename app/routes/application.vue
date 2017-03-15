e1417148a855af5f265b899f2818ba4c10dfa7c1
Evan Engels <evan.engels@gmail.com>
2017-03-15 18:49:30 -0500

    did some stuff

---
+++ app/routes/application.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="app">
+    <header class="header">
+      <div class="home">
+        <a href="/" class="home-logo"><h1>Ribbiter</h></a>
+      </div>
+      <nav class="nav">
+
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
