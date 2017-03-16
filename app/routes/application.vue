db798c4269983f44cdee2f60a476775016733bad
Evan Engels <evan.engels@gmail.com>
2017-03-16 08:56:38 -0500

    styling the navbar

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,13 +1,28 @@
 <template lang="html">
   <div class="app">
-    <header class="header">
-      <div class="home">
-        <a href="/" class="home-logo"><h1>Ribbiter</h></a>
+    <nav class="nav">
+      <div class="nav-left">
+        <a href="/" class="nav-item">
+          <p>Ribbiter</p>
+        </a>
       </div>
-      <nav class="nav">
-
-      </nav>
-    </header>
+      <div class="nav-right">
+        <a href="" class="nav-item">
+          <p>Ribblets</p>
+        </a>
+        <a href="" class="nav-item">
+          <p>Frogs</p>
+        </a>
+        <a href="" class="nav-item">
+          <p>Login</p>
+        </a>
+      </div>
+    </nav>
+    <div class="section">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
