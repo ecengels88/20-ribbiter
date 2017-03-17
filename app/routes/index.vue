87ea024040816c8f7226ed7d0303940d497f0a97
Evan Engels <evan.engels@gmail.com>
2017-03-17 08:39:34 -0500

    styled the index route

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -2,11 +2,13 @@
   <div class="">
     <div class="section">
       <div class="container">
-        <div class="welcome">
-          <h1 class="title">Welcome to Ribbiter!</h1>
-          <div class="signbtn">
-            <button class="up-btn">Sign Up</button>
-            <button class="in-btn">Sign In</button>
+        <div class="card">
+          <div class="welcome">
+            <h1 class="title">Welcome to Ribbiter!</h1>
+            <div class="signbtn">
+              <button class="btn sign-up">Sign Up</button>
+              <button class="btn sign-in">Sign In</button>
+            </div>
           </div>
         </div>
       </div>
