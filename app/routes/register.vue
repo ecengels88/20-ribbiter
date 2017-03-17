550be38261a64d2dbc01bbbf4c5601ad520f68bb
Evan Engels <evan.engels@gmail.com>
2017-03-17 09:30:43 -0500

    did some stuff with register

---
+++ app/routes/register.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="">
+    <div class="section">
+      <div class="container">
+        <div class="card">
+          <div class="register">
+            <h1 class="register__title">Create an Account</h1>
+            <form action="" class="form-inputs">
+              <div class="input">
+                <p class="input__label">Username</p>
+                <input type="text" class="input__bar username" placeholder="Username">
+              </div>
+              <div class="input">
+                <p class="input__label">Email</p>
+                <input type="text" class="input__bar email" placeholder="Email">
+              </div>
+              <div class="input">
+                <p class="input__label">Password</p>
+                <input type="text" class="input__bar password" placeholder="Password">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
