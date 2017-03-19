a9c2da7cddb62a103c2c1d81d5c18ac79163b503
Evan Engels <evan.engels@gmail.com>
2017-03-18 21:47:34 -0500

    styling the grid

--- app/routes/users-index.vue
+++ app/routes/users-index.vue
@@ -1,6 +1,69 @@
 <template lang="html">
   <div class="">
-
+    <div class="section">
+      <div class="container">
+        <div class="profile-title">
+          <h2 class="heading">Frogs</h2>
+        </div>
+        <div class="profile">
+          <div class="profile__item">
+            <div class="card">
+              <div class="card__image">
+                <img src="http://www.placecage.com/140/100" alt="" class="card-pic">
+              </div>
+              <div class="card__id">
+                <div class="name">Nicolas Cage</div>
+                <div class="user-id">Creeper69</div>
+              </div>
+            </div>
+          </div>
+          <div class="profile__item">
+            <div class="card">
+              <div class="card__image">
+                <img src="http://www.placecage.com/140/100" alt="" class="card-pic">
+              </div>
+              <div class="card__id">
+                <div class="name">Nicolas Cage</div>
+                <div class="user-id">Creeper69</div>
+              </div>
+            </div>
+          </div>
+          <div class="profile__item">
+            <div class="card">
+              <div class="card__image">
+                <img src="http://www.placecage.com/140/100" alt="" class="card-pic">
+              </div>
+              <div class="card__id">
+                <div class="name">Nicolas Cage</div>
+                <div class="user-id">Creeper69</div>
+              </div>
+            </div>
+          </div>
+          <div class="profile__item">
+            <div class="card">
+              <div class="card__image">
+                <img src="http://www.placecage.com/140/100" alt="" class="card-pic">
+              </div>
+              <div class="card__id">
+                <div class="name">Nicolas Cage</div>
+                <div class="user-id">Creeper69</div>
+              </div>
+            </div>
+          </div>
+          <div class="profile__item">
+            <div class="card">
+              <div class="card__image">
+                <img src="http://www.placecage.com/140/100" alt="" class="card-pic">
+              </div>
+              <div class="card__id">
+                <div class="name">Nicolas Cage</div>
+                <div class="user-id">Creeper69</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
